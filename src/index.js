c9e9112de6e811c96beb5b072cc8ef83898805da
Austin Clements <austinclementsbass@gmail.com>
2022-02-26 12:19:15 -0600

    add initial movie data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { MovieData, movieCollections } from './movie';
 
 ReactDOM.render(
   <React.StrictMode>
